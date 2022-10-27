e094f65e1dea74ae0fcdf4b12f33dca3755e0708
mwendamwenda <benmwenda83@gmail.com>
2022-10-27 09:35:03 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('times are getting harder');
+// console.log('times are getting harder');
+
+// const DholpinsAverrage = ((96 + 108 + 89 )/3);
+
+
+// const KoalasAverage = ((88 + 91 + 110) /3);
+
+// console.log(KoalasAverage ,DholpinsAverrage);
+
+// if(DholpinsAverrage > KoalasAverage){
+//     console.log('Dholpins wins the trophy');
+// }else if(DholpinsAverrage === KoalasAverage){
+//     console.log('they both win the trophy')
+// }else{
+//     console.log('Koalas wins the trophy');
+// }
+
+// Bonus A
+
+const DholpinsAverrage = ((97 + 112 + 101 )/3);
+
+
+const KoalasAverage = ((109 + 95 + 123) /3);
+
+console.log(KoalasAverage ,DholpinsAverrage);
+
+if(DholpinsAverrage > KoalasAverage && DholpinsAverrage >= 100){
+    console.log('Dholpins wins the trophy');
+}else if(DholpinsAverrage === KoalasAverage){
+    console.log('they both win the trophy')
+}else{
+    console.log('Koalas wins the trophy');
+}
